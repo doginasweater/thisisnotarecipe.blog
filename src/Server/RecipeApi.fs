f0ebc2e686f4e2bc0faa98693ee2cb255f9aecf0
Kevin Bernfeld <kcbernfeld@gmail.com>
2022-04-25 12:59:05 -0400

    refactoring api

--- src/Server/RecipeApi.fs
+++ src/Server/RecipeApi.fs
@@ -3,15 +3,21 @@ module RecipeApi
 open Shared
 open DbContext
 open EntityFrameworkCore.FSharp.DbContextHelpers
-open System.Linq
+
+let getRecipes () =
+    async { return! toListAsync ctx.Recipes }
+
+let getRecipe id =
+    async { return! ctx.Recipes.TryFirstAsync(fun x -> x.Id = id) }
+
+let addRecipe recipe =
+    async {
+        do! recipe |> addEntityAsync ctx
+        do! saveChangesAsync ctx
+        return recipe
+    }
 
 let recipesApi =
-    { getRecipes = fun () -> async { return! toListAsync ctx.Recipes }
-      getRecipe = fun id -> async { return! ctx.Recipes.TryFirstAsync(fun x -> x.Id = id) }
-      addRecipe =
-        fun recipe ->
-            async {
-                do! recipe |> addEntityAsync ctx
-                do! saveChangesAsync ctx
-                return recipe
-            } }
+    { getRecipes = getRecipes
+      getRecipe = getRecipe
+      addRecipe = addRecipe }
